d657d4538d8b771977a2ef2cdd83f21a5e72180b
Bilgehanaygn <bilgehanaygn@gmail.com>
2023-07-31 07:52:07 +0300

    auth process almost done, roles are missing

--- lib/api/user.ts
+++ lib/api/user.ts
@@ -4,7 +4,10 @@ import { UserViewModel } from "../models/User";
 const baseUrl = "http://localhost:8080/api/v1/user";
 
 export const getLoggedInUser = async function () {
-  const response = await axios.get<UserViewModel>(`${baseUrl}/getLoggedInUser`);
+  const response = await axios.get<UserViewModel>(
+    `${baseUrl}/getLoggedInUser`,
+    { withCredentials: true }
+  );
   if (response.data) {
     return response.data;
   }
